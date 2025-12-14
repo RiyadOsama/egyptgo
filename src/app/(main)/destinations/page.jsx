b1edd87b90dba05f73569bc230ab4fd07de74f85
Riyad Osama <riyadosama14112000@gmail.com>
2025-12-14 06:44:35 +0200

    feat: add the distinations

--- src/app/(main)/destinations/page.jsx
+++ src/app/(main)/destinations/page.jsx
@@ -1,7 +1,27 @@
+import { DestinationMainCard } from "@/components/destination-main-card";
+
 export default function DestinationsPage() {
   return (
-    <main className="max-w-7xl mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-6">Destinations</h1>
+    <main className="bg-(--background)">
+      <section className="container mx-auto py-8 md:py-16 px-4 sm:px-6 lg:px-8 ">
+        <div>
+          <div className="mb-12">
+            <h1 className="text-4xl md:text-5xl font-bold mb-4">
+              All Destinations
+            </h1>
+            <p className="text-lg text-muted-(--foreground) max-w-2xl">
+              Explore all of Egypt's incredible destinations and find your
+              perfect getaway
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <DestinationMainCard />
+          <DestinationMainCard />
+          <DestinationMainCard />
+          <DestinationMainCard />
+        </div>
+      </section>
     </main>
   );
 }
